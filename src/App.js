0395e815884e4456ad512de3a731b5ccfe66f555
Rachel Sleet <rachelsleet@gmail.com>
2019-03-16 23:28:41 +0100

    logic works and marked package included

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { MarkdownPreview } from 'react-marked-markdown';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+    this.handleInput = this.handleInput.bind(this)
+  }
+  handleInput(e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          Markdown Previewer
         </header>
+        <textarea type='text' value={this.state.input} onChange={this.handleInput}/>
+        <Previewer input={this.state.input}/>
       </div>
     );
+  }
+}
+
+
+
+class Previewer extends React.Component {
+  render() {
+    return (
+      <MarkdownPreview value={this.props.input}/>
+    )
   }
 }
 
