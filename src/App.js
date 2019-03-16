f1215152c95c8a95181422f3d0f5b33860934e7c
Rachel Sleet <rachelsleet@gmail.com>
2019-03-16 23:38:31 +0100

    logic done, styling to go

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { MarkdownPreview } from 'react-marked-markdown';
 import './App.css';
 
+let sampleInput = ` a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+
+# Header
+## Header 2
+[I work here](www.wayfair.co.uk)
+\`\`\`
+<div></div>
+\`\`\`
+- List 1
+  - List 2
+   - List 3
+
+ >Blocking a quote!
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+
+**pretty neat**
+`;
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ''
+      input: sampleInput
     }
     this.handleInput = this.handleInput.bind(this)
   }
@@ -21,7 +39,7 @@ class App extends React.Component {
         <header>
           Markdown Previewer
         </header>
-        <textarea type='text' value={this.state.input} onChange={this.handleInput}/>
+        <textarea id="editor" type='text' value={this.state.input} onChange={this.handleInput}/>
         <Previewer input={this.state.input}/>
       </div>
     );
@@ -33,7 +51,7 @@ class App extends React.Component {
 class Previewer extends React.Component {
   render() {
     return (
-      <MarkdownPreview value={this.props.input}/>
+      <MarkdownPreview id="preview" value={this.props.input}/>
     )
   }
 }
